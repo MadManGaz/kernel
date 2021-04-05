da2200f26490418533a61096644dc479c130f523
Gary Moore <madmangaz@gmail.com>
2021-04-05 15:59:47 +0100

    vga_buffer: fix some characters not appearing.
    We had the wrong range of acceptable bytes that can be printed
    to the VGA Buffer.

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -73,7 +73,6 @@ impl fmt::Write for Writer {
 }
 
 impl Writer {
-    // FIXME: Some characters get printed as unknown when they shouldn't.
     pub fn write_byte(&mut self, byte: u8) {
         match byte {
             b'\n' => self.new_line(),
@@ -99,7 +98,7 @@ impl Writer {
         for byte in s.bytes() {
             match byte {
                 // printable ASCII byte or newline
-                0x20..=0x73 | b'\n' => self.write_byte(byte),
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
                 // not part of printable ASCII range
                 _ => self.write_byte(0xfe),
             };
