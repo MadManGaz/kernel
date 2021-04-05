68b31812c0bddf2e6eadc5dea6ad0e64d00b3458
Gary Moore <madmangaz@gmail.com>
2021-04-05 06:04:19 +0100

    Add working VGA buffer and testing framework
    The VGA buffer plugs into the Rust's println!() implementation.
    This allows us to write to the screen in a very rudimentary
    way.
    
    The testing framework is pretty basic at this point, but it can
    run tests and return results! Test results are passed back to the
    host through a serial connection from QEMU, printed to stdout.

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,96 @@
-#![no_std] // Freestanding binaries can't have a stdlib
-#![no_main] // Rust main requires a runtime. We can't have that.
+// Freestanding binaries can't have a stdlib
+#![no_std]
+// Rust main requires a runtime. We can't have that.
+#![no_main]
+// The built in testing framework does not work without stdlib. We have
+// to roll our own.
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+// We have to rename the generated test runner function from main() so we
+// can call it from our custom entry point.
+#![reexport_test_harness_main = "test_main"]
+
+mod serial;
+mod vga_buffer;
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
-
-//We don't want to mangle the name as this will obfuscate what
-// function to call for the multiboot stage.
+/// Entry-point.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // This is the entry point to the program. Will be called from
-    // the multiboot assembly. 
-    let vga_buffer = 0xb8000 as *mut u8;
+    println!("Hello World{}\nVery Cool!", "!");
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    #[cfg(test)]
+    test_main();
 
-    loop { }
+    loop {}
 }
 
-// Rust requires a panic handler to be defined. This usually
-// comes from stdlib, but we don't have that here.
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+/// Custom panic handler writes to a VGA buffer rather than stdout. This panic
+/// handler is used when running normally through `cargo run`, or on a real
+/// machine.
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop { }
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+/// Panic handler for running tests in a QEMU virtual machine. Error messages
+/// are instead sent through a serial device to be displayed on the host when
+/// using `cargo test`.
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+// This is the definition of a custom testing framework for kernel
+// development.
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+/// The status code we send to our host.
+/// This is handled by the bootloader crate to return a 0 or 1 status code
+/// depending on if tests pass or not.
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+/// Should only be used when running tests. Otherwise we are making assumptions
+/// on what sort of machine we are running on.
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
