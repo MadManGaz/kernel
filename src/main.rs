e78aceb645fd61b67a4964164edb3c9b8ad1d646
Gary Moore <madmangaz@gmail.com>
2021-04-06 01:42:35 +0100

    Paging: Implement page fault handler.
    The page fault handler will print a debug message to VGA detailing the
    offending address. Not very complicated at the moment, however this will
    be used in the future to implement writing out pages to spinning rust
    when physical RAM is exhausted.
    
    This is a pretty nice abstraction normally, however this is required
    when writing an x86_64 operating system.

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,11 @@ pub extern "C" fn _start() -> ! {
 
     kernel::init();
 
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_page_table, _) = Cr3::read();
+    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+
     println!("We did not crash!");
     #[cfg(test)]
         test_main();
